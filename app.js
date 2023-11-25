77c46cff00a2c639f6e034b2571f467d1431aa8d
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-25 11:28:43 +0700

    querySelected all of the element we've to used

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const cityInput = document.querySelector('#city-input');
+// Queryselector 
+const search = document.querySelector('.search');
+const temp = document.querySelector('.temp')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
+const rain = document.querySelector('.rain')
+const uvi = document.querySelector('.uvi')
+
+// DOM
+search.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(search.elements.query.value)
+})
+
+// API requesting
 
